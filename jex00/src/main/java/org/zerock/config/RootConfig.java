3d851e00d99e98a5b4e005b56e39dd5fb981f3f1
rlarb <rlarb@DESKTOP-QIA8CO9>
2019-06-12 11:31:53 +0900

    HikariCP test, mybatis test(config : xml, java version, 86~92p)

--- jex00/src/main/java/org/zerock/config/RootConfig.java
+++ jex00/src/main/java/org/zerock/config/RootConfig.java
@@ -2,6 +2,8 @@ package org.zerock.config;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -26,6 +28,14 @@ public class RootConfig {
 		dataSource = new HikariDataSource(hikariConfig);
 		
 		return dataSource; 
+	}
+
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throws Exception{
+		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+		sqlSessionFactoryBean.setDataSource(dataSource());
+		
+		return (SqlSessionFactory) sqlSessionFactoryBean.getObject();
 	}
 	
 }
