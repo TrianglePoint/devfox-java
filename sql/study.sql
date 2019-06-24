e430c217c40041cac51301d87f0bcf09db79df0c
Kyuhyeon Kim <40159616+TrianglePoint@users.noreply.github.com>
2019-06-24 11:07:27 +0900

    ROWNUM and inline view (283 ~ 291p)

--- sql/study.sql
+++ sql/study.sql
@@ -7,3 +7,12 @@ select /*+ INDEX_DESC (tbl_board pk_board) */ * from tbl_board;
 
 -- Instead of 'order by bno asc'
 select /*+ INDEX_ASC(tbl_board pk_board) */ * from tbl_board where bno > 0;
+
+-- Show 2 page of bulletin board
+select rownum, rn, bno, title 
+from (
+  select /*+ INDEX_DESC(tbl_board pk_board) */ rownum rn, bno, title, content 
+  from tbl_board 
+  where rownum <= 20
+)
+where rn >= 11;
